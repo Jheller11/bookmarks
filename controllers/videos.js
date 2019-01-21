db4461056423bddf512ac1331dcc854c73adeed7
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-01-21 14:01:38 -0500

    finish video routes

--- controllers/videos.js
+++ controllers/videos.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const Video = require('../models/Video')
+const User = require('../models/User')
+const { isLoggedIn, isAuthor, formatTags } = require('../config/utilities')
 
 // get form
 router.get('/new', (req, res) => {
@@ -26,9 +28,27 @@ router.post('/search', (req, res) => {
     })
 })
 
-// post new video
-router.post('/', (req, res) => {
-  Video.create(req.body)
+// only videos added by an individual user
+router.get('/my_videos', isLoggedIn, (req, res) => {
+  Video.find({ createdBy: req.user.id }).then(videos => {
+    res.render('articles/index', { videos: videos })
+  })
+})
+
+// edit form
+router.get('/edit/:id', isAuthor, (req, res) => {
+  Video.findOne({ _id: req.params.id })
+    .then(video => {
+      res.render('videos/edit', { video: video })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+// view
+router.get('/:id', (req, res) => {
+  Video.findOne({ _id: req.params.id })
     .then(video => {
       res.render('videos/view', { video: video })
     })
@@ -37,16 +57,22 @@ router.post('/', (req, res) => {
     })
 })
 
-// view single video
-router.get('/:id', (req, res) => {
-  Video.findOne({ _id: req.params.id }).then(video => {
-    res.render('videos/view', { video: video })
-  })
+// update
+router.put('/:id', isAuthor, (req, res) => {
+  let newTags = formatTags(req.body.tags)
+  req.body.tags = newTags
+  Video.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(video => {
+      res.render('videos/view', { video: video })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 })
 
-// delete a video
+// delete
 router.delete('/:id', (req, res) => {
-  Video.findOneAndRemove({ _id: req.params.id })
+  Video.findOneAndDelete({ _id: req.params.id })
     .then(() => {
       res.redirect('/videos')
     })
@@ -55,7 +81,7 @@ router.delete('/:id', (req, res) => {
     })
 })
 
-// get all videos
+// index
 router.get('/', (req, res) => {
   Video.find({})
     .then(videos => {
@@ -64,6 +90,34 @@ router.get('/', (req, res) => {
     .catch(err => {
       console.log(err)
     })
+})
+
+// post
+router.post('/', isLoggedIn, (req, res) => {
+  let newTags = formatTags(req.body.tags)
+  User.findOne({ _id: req.user.id }).then(user => {
+    let userInfo = { name: user.local.displayName, id: user.id }
+    Video.create({
+      title: req.body.title,
+      youtubeId: req.body.youtubeId,
+      tags: newTags,
+      notes: req.body.notes,
+      createdBy: userInfo
+    })
+      .then(video => {
+        res.redirect('/videos')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  })
+})
+
+// 404
+router.get('/*', (req, res) => {
+  res.render('404', {
+    message: 'The page you requested does not exist. Please try again.'
+  })
 })
 
 module.exports = router
